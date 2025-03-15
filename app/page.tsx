7cbba6b29e48cf767cadf6abe7db4dcd5c3092b8
klismanmelo <klismanfdm@gmail.com>
2025-03-14 22:10:50 -0300

    video-explanations & pricing created

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
-import Image from "next/image";
 import Hero from "./components/landing-page/hero";
 import Header from "./components/landing-page/header";
+import VideoExplanation from "./components/landing-page/vide-explanation";
+import Pricing from "./components/landing-page/pricing";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto border">
       <Header />
       <Hero />
+      <VideoExplanation />
+      <Pricing />
      {/*
      
-     <VideExplaing />
-     <Pricing />
+     
      <FAQ />*/ }
     </div>
   );
