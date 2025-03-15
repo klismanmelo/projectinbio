53c76a63a809529dc885b5df8aef7bd23d474c23
klismanmelo <klismanfdm@gmail.com>
2025-03-15 16:28:34 -0300

    Poblem profileId return undefined

--- app/(pages)/[profileid]/page.tsx
+++ app/(pages)/[profileid]/page.tsx
@@ -1,15 +1,26 @@
 import ProjectCard from "@/app/components/common/project-card";
  import TotalVisits from "@/app/components/common/total-visits";
  import UserCard from "@/app/components/common/user-card";
- import { Plus } from "lucide-react";
+import { auth } from "@/app/lib/auth";
+import { getProfileData } from "@/app/server/get-profile-data";
  import Link from "next/link";
+import { notFound } from "next/navigation";
+import NewProject from "./new-project";
  
- export default async function ProfilePage({
-   params,
- }: {
+export default async function ProfilePage({
+  params,
+}: {
   params: Promise<{ profileId: string }>;
- }) {
-   const { profileId } = await params;
+}) {
+  const { profileId } = await params;
+console.log(profileId);
+   /*const profileData = await getProfileData(profileId);
+
+  if (!profileData) return notFound();
+
+  const session = await auth();
+
+  const isOwner = profileData.userId === session?.user?.id;*/
  
    return (
      <div className="relative h-screen flex p-20 overflow-hidden">
@@ -32,10 +43,7 @@ import ProjectCard from "@/app/components/common/project-card";
          <ProjectCard />
          <ProjectCard />
          <ProjectCard />
-         <button className="w-[340px] h-[132px] rounded-[20px] bg-background-secondary flex items-center gap-2 justify-center hover:border hover:border-dashed border-border-secondary">
-           <Plus className="size-10 text-accent-green" />
-           <span>Novo projeto</span>
-         </button>
+         {/*isOwner && <NewProject profileId={profileId} />*/}
        </div>
        <div className="absolute bottom-4 right-0 left-0 w-min mx-auto">
          <TotalVisits />
