900f566f23c1ae055e11e461d2bc4d07dd4c95c2
klismanmelo <klismanfdm@gmail.com>
2025-03-15 23:50:43 -0300

    create new project created, all ok

--- app/(pages)/[profileid]/page.tsx
+++ app/(pages)/[profileid]/page.tsx
@@ -43,7 +43,7 @@ export default async function ProfilePage({
          <ProjectCard />
          <ProjectCard />
          <ProjectCard />
-         {isOwner && <NewProject profileId={profileid} />}
+         {isOwner && <NewProject profileid={profileid} />}
        </div>
        <div className="absolute bottom-4 right-0 left-0 w-min mx-auto">
          <TotalVisits />
